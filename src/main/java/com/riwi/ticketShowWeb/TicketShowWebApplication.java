da96d8abcc746bbc8bfe0a9f74f11a2590d5e7cb
Juan Yepes <juanferhiguita65@gmail.com>
2024-04-24 16:23:56 -0500

    First commit

---
+++ src/main/java/com/riwi/ticketShowWeb/TicketShowWebApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.ticketShowWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketShowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketShowWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/ticketShowWeb/TicketShowWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.ticketShowWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketShowWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
