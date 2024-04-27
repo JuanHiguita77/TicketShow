c9aa8f4196fdee3eed7880f358fd0176920584f9
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-04-27 13:13:45 -0500

    #1 - Feat: Creation of the entities

---
+++ src/main/java/com/riwi/ticketShowWeb/entities/Administrator.java
@@ -0,0 +1,27 @@
+package com.riwi.ticketShowWeb.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "administrator")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Administrator {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_admin;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+
+    @Column(length = 45, nullable = false)
+    private String password;
+}
