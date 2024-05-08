a2bdf4978e72f94d4e60764d7ed9016dd970945b
Daniela_J_H <154357197+DanielaHernande@users.noreply.github.com>
2024-05-08 10:17:04 -0500

    Update EventRequest.java

--- src/main/java/com/riwi/ticketShowWeb/api/request/EventRequest.java
+++ src/main/java/com/riwi/ticketShowWeb/api/request/EventRequest.java
@@ -1,6 +1,6 @@
 package com.riwi.ticketShowWeb.api.request;
 
-import java.util.Date;
+import java.sql.Date;
 
 //import io.swagger.annotations.ApiModel;
 //import io.swagger.annotations.ApiModelProperty;
