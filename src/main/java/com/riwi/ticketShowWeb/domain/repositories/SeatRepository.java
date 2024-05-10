f01803e08f4f6fc30e9579a4aa08384a421ad329
Juan Yepes <juanferhiguita65@gmail.com>
2024-05-10 08:16:19 -0500

    Feat: Seat metods progress

--- src/main/java/com/riwi/ticketShowWeb/domain/repositories/SeatRepository.java
+++ src/main/java/com/riwi/ticketShowWeb/domain/repositories/SeatRepository.java
@@ -3,6 +3,7 @@ package com.riwi.ticketShowWeb.domain.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.riwi.ticketShowWeb.domain.entities.Seat;
@@ -10,5 +11,6 @@ import com.riwi.ticketShowWeb.domain.entities.Seat;
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> 
 {
-    
+    @Query("UPDATE Seat s SET s.available = false WHERE s.event.id = :idEvent AND s.id IN :selectedSeats")
+    void choosedSeats(Long idEvent, List<Long> selectedSeats);
 }
